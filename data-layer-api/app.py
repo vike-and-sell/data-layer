58bae04659aa5f065c81231335b22deed2ef57da
Jack Cotter <jackwcotter101@gmail.com>
2024-06-18 10:28:46 -0700

    added /create_rating

--- data-layer-api/app.py
+++ data-layer-api/app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import os
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 
@@ -12,19 +13,35 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{username}:{password}@{hos
         port=os.environ['RDS_PORT'],
         database=os.environ['RDS_DB_NAME'],
 )
+app.config['DEBUG'] = True
 
 # connect the app to the database
 db = SQLAlchemy(app)
-
-@app.route('/', methods=['GET'])
-def welcome():
-    return "Hello World"
 
 @app.route('/get_user', methods=['GET'])
 def test_sql():
     result = db.session.execute(text("SELECT * FROM Users WHERE username = 'john_doe'"))
     rows = result.fetchall()
     return str(rows)
+
+@app.post('/create_rating')
+def create_rating():
+    listing_id = request.json.get('listing_id')
+    user_id = request.json.get('user_id')
+    rating_value = request.json.get('rating')
+
+    try:
+        result = db.session.execute(text("INSERT INTO Listing_Ratings (rated_listing_id, rating_user_id, rating_value) VALUES ({}, {}, {})".format(listing_id, user_id, rating_value)))
+    except IntegrityError as e:
+        if e.orig.pgcode == '23503':
+            return jsonify({'message': 'Listing not found'}), 404
+        else:
+            return jsonify({}), 400
+    except:
+        return jsonify({'message': 'Something went wrong'}), 500
+    if result:
+        return jsonify({}), 200
+    return jsonify({'message': 'Something went wrong'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
