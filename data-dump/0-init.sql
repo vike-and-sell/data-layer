5f321d53c40c3d7cb271c6a40604c6e8ee509d4b
Jack Cotter <jackwcotter101@gmail.com>
2024-06-18 21:33:26 -0700

    Encrypted username, email, address, message_content. Altered db init to make it all happen

--- data-dump/0-init.sql
+++ data-dump/0-init.sql
@@ -1,15 +1,16 @@
 -- create_tables_and_insert_data.sql
 CREATE EXTENSION cube;
 CREATE EXTENSION earthdistance;
+CREATE EXTENSION pgcrypto;
 
 -- Create Users table
 CREATE TABLE IF NOT EXISTS Users (
     user_id SERIAL PRIMARY KEY,
-    username VARCHAR(20) NOT NULL UNIQUE CHECK (username ~ '^[a-zA-Z0-9_@]{6,20}$'),
-    email VARCHAR(100) NOT NULL UNIQUE CHECK (email ~ '^[^@]+@uvic\.ca$'),
+    username TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL CHECK (password ~ '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}$'),
     location EARTH NOT NULL,
-    address VARCHAR(100) NOT NULL,
+    address TEXT NOT NULL,
     joining_date DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     items_sold INT[] NOT NULL DEFAULT '{}',
     items_purchased INT[] NOT NULL DEFAULT '{}'
@@ -22,7 +23,7 @@ CREATE TABLE IF NOT EXISTS Listings (
     title VARCHAR(100) NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     location EARTH NOT NULL,
-    address VARCHAR(100) NOT NULL,
+    address TEXT NOT NULL,
     status VARCHAR(20) NOT NULL CHECK (status IN ('AVAILABLE', 'SOLD', 'REMOVED')),
     created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     last_updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -55,33 +56,3 @@ CREATE TABLE IF NOT EXISTS Listing_Reviews (
     review_content TEXT NOT NULL,
     created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
--- Insert dummy data into Users table
-INSERT INTO Users (username, email, password, location, address, joining_date)
-VALUES
-('john_doe', 'john_doe@uvic.ca', 'Password123!',  ll_to_earth(34.052235,118.243683), '123 Valley Lane', '2023-01-01'),
-('jane_smith', 'jane_smith@uvic.ca', 'SecurePass1$',  ll_to_earth(34.052235,-118.243683), '842 Boniface Dr', '2023-02-01');
-
--- Insert dummy data into Listings table
-INSERT INTO Listings (seller_id, title, price, location, address, status)
-VALUES
-(1, 'Bicycle for sale', 150.00, ll_to_earth(40.730610,-73.935242), '440 Kilner St', 'AVAILABLE'),
-(2, 'Laptop for sale', 800.00, ll_to_earth(34.052235,-118.243683), '892 Jonas Ave', 'AVAILABLE');
-
--- Insert dummy data into Messages table
-INSERT INTO Messages (sender_id, receiver_id, listing_id, message_content, created_on)
-VALUES
-(1, 2, 1, 'Is the bicycle still available?', '2023-03-02 10:00:00'),
-(2, 1, 2, 'Can you lower the price for the laptop?', '2023-04-02 15:30:00');
-
--- Insert dummy data into Listing Ratings table
-INSERT INTO Listing_Ratings (rated_listing_id, rating_user_id, rating_value, created_on)
-VALUES
-(1, 2, 4, '2023-03-03 12:00:00'),
-(2, 1, 5, '2023-04-03 17:45:00');
-
--- Insert dummy data into Listing Reviews table
-INSERT INTO Listing_Reviews (reviewed_listing_id, review_user_id, review_content, created_on)
-VALUES
-(1, 2, 'The bicycle was in excellent condition, very happy with the purchase!', '2023-03-03 12:05:00'),
-(2, 1, 'The laptop works perfectly, very satisfied!', '2023-04-03 17:50:00');
---
+++ data-dump/1-init-dummy-data.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# if [-z "$SECRET_KEY"]; then
+#     echo "SECRET_KEY not set"
+#     exit 1
+# fi
+
+SQL_COMMANDS=$(cat <<EOF
+-- Insert dummy data into Users table
+INSERT INTO Users (username, email, password, location, address, joining_date)
+VALUES
+(pgp_sym_encrypt('john_doe', '${ENCRYPTION_KEY}'), pgp_sym_encrypt('john_doe@uvic.ca', '${ENCRYPTION_KEY}'), 'Password123!',  ll_to_earth(34.052235,118.243683), pgp_sym_encrypt('123 Valley Lane', '${ENCRYPTION_KEY}'), '2023-01-01'),
+(pgp_sym_encrypt('jane_smith', '${ENCRYPTION_KEY}'), pgp_sym_encrypt(jane_smith@uvic.ca', '${ENCRYPTION_KEY}')', 'SecurePass1$',  ll_to_earth(34.052235,-118.243683), pgp_sym_encrypt('842 Boniface Dr', '${ENCRYPTION_KEY}'), '2023-02-01');
+
+-- Insert dummy data into Listings table
+INSERT INTO Listings (seller_id, title, price, location, address, status)
+VALUES
+(1, 'Bicycle for sale', 150.00, ll_to_earth(40.730610,-73.935242), pgp_sym_encrypt('440 Kilner St', '${ENCRYPTION_KEY}'), 'AVAILABLE'),
+(2, 'Laptop for sale', 800.00, ll_to_earth(34.052235,-118.243683), pgp_sym_encrypt('892 Jonas Ave', '${ENCRYPTION_KEY}'), 'AVAILABLE');
+
+-- Insert dummy data into Messages table
+INSERT INTO Messages (sender_id, receiver_id, listing_id, message_content, created_on)
+VALUES
+(1, 2, 1, pgp_sym_encrypt('Is the bicycle still available?', '${ENCRYPTION_KEY}'), '2023-03-02 10:00:00'),
+(2, 1, 2, pgp_sym_encrypt('Can you lower the price for the laptop?', '${ENCRYPTION_KEY}'), '2023-04-02 15:30:00');
+
+-- Insert dummy data into Listing Ratings table
+INSERT INTO Listing_Ratings (rated_listing_id, rating_user_id, rating_value, created_on)
+VALUES
+(1, 2, 4, '2023-03-03 12:00:00'),
+(2, 1, 5, '2023-04-03 17:45:00');
+
+-- Insert dummy data into Listing Reviews table
+INSERT INTO Listing_Reviews (reviewed_listing_id, review_user_id, review_content, created_on)
+VALUES
+(1, 2, 'The bicycle was in excellent condition, very happy with the purchase!', '2023-03-03 12:05:00'),
+(2, 1, 'The laptop works perfectly, very satisfied!', '2023-04-03 17:50:00');
+EOF
+)
+
+echo "$SQL_COMMANDS" | psql -U "$POSTGRES_USER" -d "$POSTGRES_DB"
