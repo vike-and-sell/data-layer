08b2bf37c086174a9b82c5f32e759bf0af27e3eb
Brennan McMicking <brennanmcmicking@gmail.com>
2024-06-24 16:31:19 -0700

    remove: at-rest encryption logic

--- data-dump/1-init-dummy-data.sh
+++ data-dump/1-init-dummy-data.sh
@@ -15,8 +15,8 @@ VALUES
 -- Insert dummy data into Listings table
 INSERT INTO Listings (seller_id, title, price, location, address, status)
 VALUES
-(1, 'Bicycle for sale', 150.00, ll_to_earth(40.730610,-73.935242), pgp_sym_encrypt('440 Kilner St', '${ENCRYPTION_KEY}'), 'AVAILABLE'),
-(2, 'Laptop for sale', 800.00, ll_to_earth(34.052235,-118.243683), pgp_sym_encrypt('892 Jonas Ave', '${ENCRYPTION_KEY}'), 'AVAILABLE');
+(1, 'Bicycle for sale', 150.00, ll_to_earth(40.730610,-73.935242), '440 Kilner St', 'AVAILABLE'),
+(2, 'Laptop for sale', 800.00, ll_to_earth(34.052235,-118.243683), '892 Jonas Ave', 'AVAILABLE');
 
 -- Insert dummy data into Chats table
 INSERT INTO Chats (seller, buyer, listing_id)
@@ -27,8 +27,8 @@ VALUES
 -- Insert dummy data into Messages table
 INSERT INTO Messages (chat_id, sender_id, message_content, created_on)
 VALUES
-(2, 1, pgp_sym_encrypt('Is the bicycle still available?', '${ENCRYPTION_KEY}'), '2023-03-02 10:00:00'),
-(1, 2, pgp_sym_encrypt('Can you lower the price for the laptop?', '${ENCRYPTION_KEY}'), '2023-04-02 15:30:00');
+(2, 1, 'Is the bicycle still available?', '2023-03-02 10:00:00'),
+(1, 2, 'Can you lower the price for the laptop?', '2023-04-02 15:30:00');
 
 -- Insert dummy data into Listing Ratings table
 INSERT INTO Listing_Ratings (rated_listing_id, rating_user_id, rating_value, created_on)
